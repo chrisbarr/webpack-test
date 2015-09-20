ff1a93cbc482333e98d5f343ea7b345bb30951ec
Chris Barr <chris.barr@ntlworld.com>
2015-09-20 20:44:36 +0100

    Working import with requirejs and importing jquery - not using webpack

---
+++ content.js
@@ -0,0 +1,9 @@
+define(function(){
+  var test = function() {
+    console.log('This is a test');
+  }
+
+  return {
+    test: test
+  };
+});
---
+++ entry.js
@@ -0,0 +1,12 @@
+requirejs.config({
+  "baseUrl": "./",
+  "paths": {
+    "jquery": "bower_components/jquery/dist/jquery"
+  }
+});
+
+define(['./content', 'jquery'], function(content, $) {
+  content.test();
+
+  $('p').text('test');
+});
